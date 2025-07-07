2f09c9cf8af79c0778a7e8e946f80398c35b8497
Pubg121205 <86605975+Pubg121205@users.noreply.github.com>
2025-07-07 21:56:22 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/pubg121205.github.io/', // đổi nếu repo GitHub của bạn có tên khác
-});
